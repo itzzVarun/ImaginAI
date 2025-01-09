2c9863b48d69ff41d49fb82539e67bfd8d41dcdb
itzzVarun <itsrealvarun@gmail.com>
2025-01-10 01:16:27 +0530

    Final Commit

--- aiFrontend/src/lib/axios.js
+++ aiFrontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL:
-    import.meta.env.MODE === "development"
+    process.env.NODE_ENV === "development"
       ? "http://localhost:8090/api/v1"
       : "/api/v1",
   withCredentials: true,
