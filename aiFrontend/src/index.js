b2c006575c974d3e299a1ea04f49b7aa72cc9db9
itzzVarun <itsrealvarun@gmail.com>
2025-01-14 18:42:06 +0530

    Final Commit

--- aiFrontend/src/index.js
+++ aiFrontend/src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
@@ -6,8 +6,31 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { AuthProvider } from "./AuthContext/AuthContext";
+
+function Root() {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 4000); // Adjust time based on your hosting service delay
+
+    return () => clearTimeout(timer); // Cleanup on unmount
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div style={{ textAlign: "center", marginTop: "20%" }}>
+        <h1>
+          This website is hosted on a free plan. Kindly wait sometime for
+          website to load.
+        </h1>
+      </div>
+    );
+  }
+
+  return <App />;
+}
 //Stripe configuration
 const stripePromise = loadStripe(
   "pk_test_51QcKDiC0r8A6M19asUrBtyH1DKvr0AQxUyGdHgEnieedTYL6fWm4oB9r7G03SMFsQg3VlR0vJNPruImTQIl7u9S100k0QCxvxw"
@@ -27,7 +50,7 @@ root.render(
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <Elements stripe={stripePromise} options={options}>
-          <App />
+          <Root />
         </Elements>
       </AuthProvider>
       <ReactQueryDevtools initialIsOpen={false} />
