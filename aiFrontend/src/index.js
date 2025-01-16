177a730f4efaa864e9734ff590739d5475b5da41
itzzVarun <itsrealvarun@gmail.com>
2025-01-16 11:16:56 +0530

    Final Commit

--- aiFrontend/src/index.js
+++ aiFrontend/src/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
@@ -8,29 +8,6 @@ import "./index.css";
 import App from "./App";
 import { AuthProvider } from "./AuthContext/AuthContext";
 
-function Root() {
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 4000); // Adjust time based on your hosting service delay
-
-    return () => clearTimeout(timer); // Cleanup on unmount
-  }, []);
-
-  if (isLoading) {
-    return (
-      <div style={{ textAlign: "center", marginTop: "20%" }}>
-        <h1>
-          This website is hosted on a free plan. Kindly wait sometime for
-          website to load.
-        </h1>
-      </div>
-    );
-  }
-
-  return <App />;
-}
 //Stripe configuration
 const stripePromise = loadStripe(
   "pk_test_51QcKDiC0r8A6M19asUrBtyH1DKvr0AQxUyGdHgEnieedTYL6fWm4oB9r7G03SMFsQg3VlR0vJNPruImTQIl7u9S100k0QCxvxw"
@@ -50,7 +27,7 @@ root.render(
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <Elements stripe={stripePromise} options={options}>
-          <Root />
+          <App />
         </Elements>
       </AuthProvider>
       <ReactQueryDevtools initialIsOpen={false} />
